7cf3b6ba0d926d68a85c9041343b350bf34e6381
Kenny Cassandra <kennycassandra@outlook.com>
2019-02-28 16:45:24 +0700

    oke

---
+++ BTPNS.Scheduler/BTPNS.Scheduler/RDLCHelper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTPNS.Scheduler
+{
+    public class RDLCHelper
+    {
+         public string GeneratePDF(string RDLC, string OutputFileName)
+         {  
+            // select appropriate contenttype, while binary transfer it identifies filetype  
+             string contentType = string.Empty;
+            contentType = "application/pdf";
+             //if (ddlFileFormat.SelectedValue.Equals(".pdf"))  
+             //    contentType = "application/pdf";  
+             //if (ddlFileFormat.SelectedValue.Equals(".doc"))  
+             //    contentType = "application/ms-word";  
+             //if (ddlFileFormat.SelectedValue.Equals(".xls"))  
+             //    contentType = "application/xls";  
+   
+             DataTable dsData = new DataTable();  
+             dsData = getReportData();  
+   
+             string FileName = OutputFileName;  
+             string extension;  
+             string encoding;  
+             string mimeType;  
+             string[] streams;  
+             Warning[] warnings;  
+   
+             LocalReport report = new LocalReport();  
+             report.ReportPath = Path.Combine(Environment.CurrentDirectory, "RDLC") + "/" + RDLC;
+            ReportDataSource rds = new ReportDataSource();  
+             rds.Name = "DataSet1";//This refers to the dataset name in the RDLC file  
+             rds.Value = dsData;  
+             report.DataSources.Add(rds);  
+   
+             Byte[] mybytes = report.Render("PDF", null,
+                    out extension, out encoding,
+                    out mimeType, out streams, out warnings); //for exporting to PDF  
+            string file_output_url = Path.Combine(Environment.CurrentDirectory, "Output") + FileName;
+
+             using (FileStream fs = File.Create(file_output_url))  
+             {  
+                 fs.Write(mybytes, 0, mybytes.Length);  
+             }
+
+            return file_output_url;
+        }  
+
+    }
+}
---
+++ BTPNS.Scheduler/BTPNS.Scheduler/SPOnlineHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using System.Security;
+using System.Configuration;
+namespace BTPNS.Scheduler
+{
+    public class SPOnlineHelper
+    {
+        public void UploadFile(string SourceFile, string OutputFileName)
+        {
+            try
+            {
+                using (ClientContext cnx = new ClientContext("https://test.sharepoint.com/sites/test01"))
+                {
+                    string password = ConfigurationManager.AppSettings["PassAccount"].ToString();
+                    string account = ConfigurationManager.AppSettings["LoginAccount"].ToString();
+                    var secret = new SecureString();
+                    foreach (char c in password)
+                    {
+                        secret.AppendChar(c);
+                    }
+                    cnx.Credentials = new SharePointOnlineCredentials(account, secret);
+
+                    Web web = cnx.Web;
+
+                    FileCreationInformation newFile = new FileCreationInformation();
+                    newFile.Content = System.IO.File.ReadAllBytes("document.pdf");
+
+                    //file url is name
+                    newFile.Url = @"document.pdf";
+                    List docs = web.Lists.GetByTitle("Contact");
+
+                    //get folder and add to that
+                    Folder folder = docs.RootFolder.Folders.GetByUrl("demo");
+                    File uploadFile = folder.Files.Add(newFile);
+
+                    cnx.Load(docs);
+                    cnx.Load(uploadFile);
+                    cnx.ExecuteQuery();
+                    Console.WriteLine("done UploadFile");
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
